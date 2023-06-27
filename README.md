8c65034142184da6c93a5eeaf3d750c2a0d0a1be
hyeonbeen97 <125417850+hyeonbeen97@users.noreply.github.com>
2023-06-27 09:47:35 +0900

    first commit

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # Getting Started with Create React App
 
-# 유튜브 API를 이용한 사이트 입니다.
+# 유튜브 API를 이용한 사이트 입니다!
--- src/components/MainConts.jsx
+++ src/components/MainConts.jsx
@@ -1,8 +1,19 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Category, Videos } from "./";
 
 const MainConts = () => {
   const [selectCategory, setSelectCategory] = useState("추천");
+  const [youtubes, setYoutubes] = useState([]);
+
+  useEffect(() => {
+    fetch(
+      // "https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=28&q=프로그래머&key=AIzaSyBmD-yNiyzU1muLrsF_Gg7FKf5HgyXfxlo",
+      "https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=28&q=프로그래머&key=AIzaSyBTyYekqKdJKfjwf6aUYLwTbtRLG_jR2H8"
+    )
+      .then((response) => response.json())
+      .then((result) => setYoutubes(result.items))
+      .catch((error) => console.log(error));
+  }, []);
   return (
     <main id="main">
       <aside id="aside">
@@ -13,7 +24,7 @@ const MainConts = () => {
       </aside>
       <section id="contents">
         <h2>{selectCategory} 유튜버</h2>
-        <Videos />
+        <Videos youtubes={youtubes} />
       </section>
     </main>
   );
